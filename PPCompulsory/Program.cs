f5bfedb099e858e6d6539887a8c981c263a4653c
robie sun <robie.sun.rs@gmail.com>
2019-10-10 14:08:37 +0200

    [web application-2] GUID for Prime number

--- PPCompulsory/Program.cs
+++ PPCompulsory/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace PrimeNumber
+namespace PPCompulsory
 {
     public class Program
     {
