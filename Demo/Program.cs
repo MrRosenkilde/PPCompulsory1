259b56a4f89638a05f91171c3bdf58816e6e0ce4
Dennis <dennis@rosenkilde.nu>
2019-10-10 23:20:33 +0200

    GetPrimesParallel optimized

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,36 +1,39 @@
 ﻿using PPCompulsory1.Logic;
 using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Demo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var pg = new PrimeGenerator();
             var int_pairs = new int_pair[]
             {
-                new int_pair { Int1 = 0, Int2 = 1_000 },
-                //new int_pair { Int1 = 0, Int2 = 10_000_000 },
-                //new int_pair { Int1 = 1_000_000, Int2 = 2_000_000 },
-                //new int_pair { Int1 = 10_000_000, Int2 = 20_000_000 },
-                //new int_pair { Int1 = 100_000_000, Int2 = 200_000_000 },
+                //new int_pair { Int1 = 0, Int2 = 1_000 },
+                new int_pair { Int1 = 0, Int2 = 1_000_000 },
+                new int_pair { Int1 = 1_000_000, Int2 = 2_000_000 },
+                new int_pair { Int1 = 0, Int2 = 10_000_000 },
+                new int_pair { Int1 = 10_000_000, Int2 = 20_000_000 },
+                new int_pair { Int1 = 0, Int2 = 100_000_000 },
+                new int_pair { Int1 = 100_000_000, Int2 = 200_000_000 },
+                new int_pair { Int1 = 0, Int2 = 1_000_000_000 },
             };
             foreach (int_pair ip in int_pairs)
             {
-                var parallel = pg.SieveOfEratosthenesParallel(ip.Int1, ip.Int2);
-                var sequentiel = pg.GetPrimesSequential(ip.Int1, ip.Int2);
+                CompareFunctions(
+                    //a1: () => Task.WaitAll( pg.GetPrimesParallel(ip.Int1,ip.Int2) ),
+                    a1: () => { var a = pg.GetPrimesParallel(ip.Int1, ip.Int2).Result; },
+                    a2: () => { var b = pg.GetPrimesSequential(ip.Int1, ip.Int2).Result; },
+                    string.Format("GetPrimesParallel({0:n0},{1:n0})", ip.Int1, ip.Int2),
+                    string.Format("GetPrimesSequential({0:n0},{1:n0})", ip.Int1, ip.Int2)
+                );
+                //var parallel = pg.GetPrimesParallel(ip.Int1, ip.Int2);
+                //var sequentiel = pg.GetPrimesSequential(ip.Int1, ip.Int2);
             }
-            //CompareFunctions(
-            //    //a1: () => Task.WaitAll( pg.GetPrimesParallel(ip.Int1,ip.Int2) ),
-            //    a1: () => pg.SieveOfEratosthenesParallel(ip.Int1,ip.Int2),
-            //    a2: () => pg.GetPrimesSequential(ip.Int1,ip.Int2),
-            //    string.Format("GetPrimesParallel({0:n},{1:n})",ip.Int1,ip.Int2),
-            //    string.Format("GetPrimesSequential({0:n},{1:n})",ip.Int1,ip.Int2)
-
-            //);
         }
         static void CompareFunctions(Action a1, Action a2,string a1name,string a2name)
         {
@@ -38,12 +41,12 @@ namespace Demo
             sw.Start();
             a1.Invoke();
             sw.Stop();
-            Console.WriteLine("{0} executed in {1:0,0}ms",a1name,sw.ElapsedMilliseconds);
+            Console.WriteLine("{0} executed in {1:n0}ms",a1name,sw.ElapsedMilliseconds);
             var sw2 = new Stopwatch();
             sw2.Start();
             a2.Invoke();
             sw2.Stop();
-            Console.WriteLine("{0} executed in {1:0,0}ms", a2name, sw2.ElapsedMilliseconds);
+            Console.WriteLine("{0} executed in {1:n0}ms", a2name, sw2.ElapsedMilliseconds);
             Console.WriteLine();
 
         }
